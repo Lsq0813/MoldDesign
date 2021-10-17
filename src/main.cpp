f4f1cb20917030fdcfd639f6fe1a28f11b2bfea8
laityjet <laityjet@gmail.com>
2021-10-17 11:15:11 +0800

    简化代码结构

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,17 @@
 #include "product.h"
 #include "dl_jww-copy.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    if(argc < 2)
+    {
+        std::cout<<"file path as parameter\n";
+        return 1;
+    }
     std::cout<<"MoldDesign start ...\n";
     product p;
     DL_Jww b;
-    string file("../dxf/circle.dxf");
+    string file(argv[1]);
     b.in(file, &p);
     p.print();
     std::cout<<"MoldDesign end\n";
